15f4b8c24a40925a8cc60555b383ddc304546180
ArmandoAVP <armandovipa@gmail.com>
2026-05-08 16:36:21 +0200

    añadido sesiones del carrito y pagina del carrito

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ import { fileURLToPath } from 'node:url'
 import nunjucks from 'nunjucks'
 import dotenv from 'dotenv'
 import methodOverride from 'method-override'
+import session from 'express-session'
 
 
 import connectMongo from './config/mongoose.js'
 import indexRouter from './routes/contactos.js'
 import filmRouter from './routes/films.js'
-import detailRouter from './routes/details.js'
+import detailsRouter from './routes/details.js'
 import cartRouter from './routes/cart.js'
 import { viteAsset, viteCssFiles, isDev } from './utils/vite-assets.js'
 
@@ -23,6 +24,13 @@ connectMongo()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(session({
+    secret: process.env.SECRETO || 'clave_secreta_carrito',
+    resave: false,
+    saveUninitialized: false
+}));
+
 
 app.use('/build', express.static(path.resolve(process.cwd(), 'public/build')))
 app.use(express.static(path.resolve(process.cwd(), 'public')))
@@ -48,9 +56,14 @@ app.locals.isDev = isDev
 app.locals.viteAsset = viteAsset
 app.locals.viteCssFiles = viteCssFiles
 
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
+
 app.use('/contactos', indexRouter)
 app.use('/films', filmRouter)
-app.use('/details', detailRouter)
+app.use('/details', detailsRouter)
 app.use('/cart', cartRouter)
 
 const port = process.env.PORT || 3000
